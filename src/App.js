ceece1d1605f165fe359cac2072665c5c498c083
viadel13 <shababizetre@gmail.com>
2022-12-15 21:31:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Route} from 'react-router-dom';
+import { Switch, Route} from 'react-router-dom';
 import Console from './composants/Console';
 import Accueil from './composants/Accueil';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Route exact path="/" component={Accueil}></Route>
-        <Route path="/Console" component={Console}></Route>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Accueil />
+        </Route>
+        <Route path="/Console">
+          <Console />
+        </Route>
+      </Switch>
     )
   }
 }
