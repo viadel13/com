ffcd1a7a12ae016ddf2a87926d0a252e5fdc18fe
viadel13 <shababizetre@gmail.com>
2022-12-15 22:12:44 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import { HashRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './styles/styles.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
